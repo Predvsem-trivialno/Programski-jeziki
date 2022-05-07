d8b20efebf301a8cc75e78d1dcee0ed8c658ab06
xmapo <matejp@yahoo.com>
2022-05-07 20:10:18 +0200

    Prepared QR code scanner and login activity

---
+++ app/src/main/java/com/example/pj_projekt/LoginActivity.kt
@@ -0,0 +1,14 @@
+package com.example.pj_projekt
+
+import android.os.Bundle
+import com.example.pj_projekt.databinding.ActivityLoginBinding
+
+class LoginActivity : BaseActivity() {
+    private lateinit var binding: ActivityLoginBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
--- app/src/main/java/com/example/pj_projekt/MainActivity.kt
+++ app/src/main/java/com/example/pj_projekt/MainActivity.kt
@@ -1,11 +1,46 @@
 package com.example.pj_projekt
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
+import android.os.Build
 import android.os.Bundle
+import android.view.HapticFeedbackConstants
+import android.widget.Toast
+import androidx.annotation.RequiresApi
+import com.example.pj_projekt.databinding.ActivityMainBinding
+import com.google.gson.Gson
+import com.google.zxing.integration.android.IntentIntegrator
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : BaseActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        binding.btnScanQR.setOnClickListener{
+            IntentIntegrator(this).initiateScan()
+        }
+    }
+
+    @Deprecated("Deprecated in Java")
+    @RequiresApi(Build.VERSION_CODES.R)
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        val result = IntentIntegrator.parseActivityResult(requestCode,resultCode,data)
+        if(result!=null){
+            if(result.contents==null){
+                Toast.makeText(applicationContext,"Cancelled", Toast.LENGTH_LONG).show()
+            } else {
+                try{
+                    val json = result.contents
+                    val gson = Gson()
+                    val id = gson.fromJson(json, String::class.java)
+                    Toast.makeText(applicationContext,"Scanned postbox with id: $id", Toast.LENGTH_SHORT).show()
+                } catch (e: Exception){
+                    binding.root.performHapticFeedback(HapticFeedbackConstants.REJECT)
+                    Toast.makeText(applicationContext,"Invalid QR code", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
     }
 }
